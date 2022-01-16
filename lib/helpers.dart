b41ee56860014ccde228450e4457068ba9668ebb
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2022-01-16 10:18:47 -0500

    Se modifica helpers.dart para leer nodos que tienen repetida la etiqueta data key

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -20,14 +20,18 @@ List<NodeFlowDiagram> _readNodes(XmlDocument document) {
   List<NodeFlowDiagram> nodes = [];
   for (var node in document.findAllElements('node')) {
     var id = node.attributes.single.value;
-    var height = double.parse(
-        node.children[1].children[1].children[1].attributes[0].value);
-    var width = double.parse(
-        node.children[1].children[1].children[1].attributes[1].value);
-    var x = double.parse(
-        node.children[1].children[1].children[1].attributes[2].value);
-    var y = double.parse(
-        node.children[1].children[1].children[1].attributes[3].value);
+    int indexDataKey = 1;
+    var dataKeyChild = node.children[indexDataKey];
+    while (dataKeyChild.children.isEmpty) {
+      indexDataKey++;
+      dataKeyChild = node.children[indexDataKey];
+    }
+    var geometryChild = dataKeyChild.children[1].children[1];
+
+    var height = double.parse(geometryChild.attributes[0].value);
+    var width = double.parse(geometryChild.attributes[1].value);
+    var x = double.parse(geometryChild.attributes[2].value);
+    var y = double.parse(geometryChild.attributes[3].value);
 
     nodes.add(NodeFlowDiagram(
       id: id,
--- test/helpers_test.dart
+++ test/helpers_test.dart
@@ -6,10 +6,10 @@ import 'package:xml/xml.dart';
 import 'fixtures/fixture_reader.dart';
 
 void main() {
-  XmlDocument testDoc = XmlDocument.parse(fixture('yed_diagram.graphml'));
   late FlowDiagram flowDiagram;
 
   group('parseDiagram', () {
+    XmlDocument testDoc = XmlDocument.parse(fixture('yed_diagram.graphml'));
     setUp(() {
       flowDiagram = parseDiagram(testDoc);
     });
@@ -23,6 +23,33 @@ void main() {
         () {
       //var flowDiagram = parseDiagram(testDoc);
       expect(flowDiagram.nodes.length, 9);
+    });
+    test(
+        'parseDiagram, al menos un nodo debe tener coordenada x=0, y al menos un nodo debe tener y=0 (no necesariamente el mismo nodo)',
+        () {
+      expect(flowDiagram.nodes.firstWhere((element) => element.x == 0),
+          isA<NodeFlowDiagram>());
+      expect(flowDiagram.nodes.firstWhere((element) => element.y == 0),
+          isA<NodeFlowDiagram>());
+    });
+  });
+
+  group('parseDiagram con doble data key en los nodos', () {
+    XmlDocument testDoc =
+        XmlDocument.parse(fixture('yed_diagram_doble_data_node.graphml'));
+    setUp(() {
+      flowDiagram = parseDiagram(testDoc);
+    });
+    test('parseDiagram debe retornar una instancia de FlowDiagram', () {
+      //var flowDiagram = parseDiagram(testDoc);
+      expect(flowDiagram, isA<FlowDiagram>());
+    });
+
+    test(
+        'parseDiagram debe retornar una instancia de FlowDiagram con veintidos nodos',
+        () {
+      //var flowDiagram = parseDiagram(testDoc);
+      expect(flowDiagram.nodes.length, 22);
     });
     test(
         'parseDiagram, al menos un nodo debe tener coordenada x=0, y al menos un nodo debe tener y=0 (no necesariamente el mismo nodo)',
